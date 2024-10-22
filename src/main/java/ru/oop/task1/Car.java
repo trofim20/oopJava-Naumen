dee5636e3487b5dfdb4a92d1af1d6997e9dc27f9
ntrofim20 <ntrofim35@mail.ru>
2024-10-22 15:04:08 +0500

    Сделал 1 задачу

---
+++ src/main/java/ru/oop/task1/Car.java
@@ -0,0 +1,18 @@
+package ru.oop.task1;
+/**
+ * Машина, на которой едет человек
+ * */
+public class Car {
+    /**
+     * Конструктор
+     * @param person
+     * */
+    public Car(Person person) {
+    }
+    /**
+     * Доехать до места назначения
+     * @param destination место назначения
+     * */
+    public void drive(Position destination) {
+    }
+}
